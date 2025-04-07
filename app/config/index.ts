1bcf393113ee726fcde7696b8b4a74d29d238e72
zyuvszqk <zumwachang479@gmail.com>
2025-04-07 09:06:21 +0800

    project config

--- app/config/index.ts
+++ app/config/index.ts
@@ -25,7 +25,7 @@ import {
   useWalletInfo
 } from '@reown/appkit/react'
 
-export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID || 'b56e18d47c72ab683b10814fe9495694' // this is a public projectId only to use on localhost
+export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID// this is a public projectId only to use on localhost
 
 export const networks = [
   mainnet,
